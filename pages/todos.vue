7acdbbb111a3c22eb0b2fe4e6ad46505f2297232
Tatsuya <yokokura0413@gmail.com>
2021-03-27 19:06:23 +0900

    remove: 不要なフィアル

---
+++ pages/todos.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <form @submit.prevent="add">
+      <input v-model="name" />
+      <button>add</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: '',
+      done: false,
+    }
+  },
+  created() {
+    this.$store.dispatch('todos/init')
+  },
+  methods: {
+    add() {
+      this.$store.dispatch('todos/add', this.name)
+      this.name = ''
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
